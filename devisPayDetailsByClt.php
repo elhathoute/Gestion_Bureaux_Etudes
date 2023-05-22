cfffe4ce5e64389ba387cf443cf2572ea039d68e
RedOuaneAO <redouaneao97@gmail.com>
2023-05-22 01:24:25 +0100

    filter

--- devisPayDetailsByClt.php
+++ devisPayDetailsByClt.php
@@ -13,7 +13,7 @@ while ($row = mysqli_fetch_assoc($res)) {
     $subarray[] = $row['client'];
     $subarray[] = ($row['N_dossier'] != NULL) ? $row['N_dossier'] : '-';
     $subarray[] = $row['service_name'];
-    $subarray[] = 'Qte=' . $row['quantity'] . ' count_dossier=' . getCountDossierService($row['srv_id']);
+    $subarray[] = $row['quantity'];
     $subarray[] = $row['srv_prix'];
     $subarray[] = $row['solde'];
     $subarray[] = '<input type="checkbox" name="ids[]" class="CBPaymentByClient" value="'.$row["srv_id"].'"><input type="checkbox" name="devis[]" class="DevisCheckBox d-none " value="'.$row["id"].'"><input type="checkbox" name="dossiers[]" class="DossierCheckBox d-none" value="'.$row["id_dossier"].'"><input type="hidden" name="invoiceId[]" class="" value="'.$row["srv_id"].'">';
